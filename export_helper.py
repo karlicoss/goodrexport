dfcbdc0234da990c5b663c4eabf0e91dcedb979f
Dima Gerasimov <karlicoss@gmail.com>
2020-01-24 12:41:34 +0000

    CI, readme and package structure updates

--- export_helper.py
+++ export_helper.py
@@ -1,10 +1,19 @@
 import argparse
 from pathlib import Path
 import sys
-from typing import Sequence, Dict, Any, Optional
+from typing import Sequence, Dict, Any, Optional, Union
 
 
 Json = Dict[str, Any]
+
+
+def Parser(*args, **kwargs):
+    # just more reasonable default for literate usage
+    return argparse.ArgumentParser( # type: ignore[misc]
+        *args,
+        formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=100), # type: ignore
+        **kwargs,
+    )
 
 
 def setup_parser(parser: argparse.ArgumentParser, *, params: Sequence[str], extra_usage: Optional[str]=None):
@@ -67,7 +76,7 @@ def setup_parser(parser: argparse.ArgumentParser, *, params: Sequence[str], extr
     paramss = ' '.join(f'--{p} <{p}>' for p in params)
     # TODO extract export programmatically?
 
-    sep = '\n:    '
+    sep = '\n: '
     secrets_example = sep + sep.join(f'{p} = "{p.upper()}"' for p in params)
 
     parser.epilog = f'''
@@ -75,18 +84,30 @@ Usage:
 
 *Recommended*: create =secrets.py= keeping your api parameters, e.g.:
 
-    {secrets_example}
+{secrets_example}
 
 
-After that, use: ~./export.py --secrets /path/to/secrets.py~
-That way you type less and have control over where you keep your plaintext tokens/passwords.
+After that, use:
 
-*Alternatively*, you can pass parameters directly, e.g. ~./export.py {paramss}~.
-However, this is verbose and prone to leaking your keys in shell history.
+: ./export.py --secrets /path/to/secrets.py
+
+That way you type less and have control over where you keep your plaintext secrets.
+
+*Alternatively*, you can pass parameters directly, e.g.
+
+: ./export.py {paramss}
+
+However, this is verbose and prone to leaking your keys/tokens/passwords in shell history.
+
     '''
 
     if extra_usage is not None:
         parser.epilog += extra_usage
+
+    parser.epilog += '''
+
+I *highly* recommend checking exported files at least once just to make sure they contain everything you expect from your export. If not, please feel free to ask or raise an issue!
+    '''
 
     parser.add_argument(
         '--secrets',
@@ -107,3 +128,37 @@ However, this is verbose and prone to leaking your keys in shell history.
         nargs='?',
         help='Optional path where exported data will be dumped, otherwise printed to stdout',
     )
+
+
+import logging
+def setup_logger(logger: Union[str, logging.Logger], level='DEBUG', **kwargs):
+    """
+    Wrapper to simplify logging setup.
+    """
+    def mklevel(level: Union[int, str]) -> int:
+        if isinstance(level, str):
+            return getattr(logging, level)
+        else:
+            return level
+    lvl = mklevel(level)
+
+    if isinstance(logger, str):
+        logger = logging.getLogger(logger)
+
+    try:
+        # try logzero first, so user gets nice colored logs
+        import logzero  # type: ignore
+        # TODO meh, default formatter shorthands logging levels making it harder to search errors..
+    except ModuleNotFoundError:
+        import warnings
+        warnings.warn("You might want to install 'logzero' for nice colored logs")
+
+        # ugh. why does it have to be so verbose?
+        logger.setLevel(lvl)
+        ch = logging.StreamHandler()
+        ch.setLevel(lvl)
+        FMT = '[%(levelname)s %(name)s %(asctime)s %(filename)s:%(lineno)d] %(message)s'
+        ch.setFormatter(logging.Formatter(FMT))
+        logger.addHandler(ch)
+    else:
+        logzero.setup_logger(logger.name, level=lvl)
